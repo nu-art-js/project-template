137732b324c782a8d1e20a7c699718c506e33ae3
TacB0sS <adam.vdk@nu-art-software.com>
2020-08-02 19:14:01 +0300

    wip

---
+++ thunderstorm/.eslintrc.js
@@ -0,0 +1,51 @@
+/*
+ * Permissions management system, define access level for each of
+ * your server apis, and restrict users by giving them access levels
+ *
+ * Copyright (C) 2020 Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module.exports = {
+	parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+	parserOptions: {
+		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+		sourceType: "module", // Allows for the use of imports
+		ecmaFeatures: {
+			jsx: true // Allows for the parsing of JSX
+		}
+	},
+	settings: {
+		react: {
+			version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+		}
+	},
+	extends: [
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended"// Uses the recommended rules from @typescript-eslint/eslint-plugin
+	],
+	rules: {
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/camelcase": "off",
+		"@typescript-eslint/ban-ts-comment": "off",
+		"@typescript-eslint/ban-ts-ignore": "off",
+		"@typescript-eslint/no-use-before-define": "off",
+		"@typescript-eslint/no-empty-function": "off",
+		"@typescript-eslint/class-name-casing": "off",
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/explicit-function-return-type": "off",
+		"@typescript-eslint/explicit-module-boundary-types": "off"
+	},
+};
